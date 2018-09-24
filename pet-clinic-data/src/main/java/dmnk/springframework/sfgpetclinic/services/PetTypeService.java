b65aa6557d4e784f56ae3fef92445babbb16aca7
dmnksdr <dmnksdr@gmail.com>
2018-09-24 13:13:51 +0200

    Add PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/dmnk/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package dmnk.springframework.sfgpetclinic.services;
+
+import dmnk.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+
+}
