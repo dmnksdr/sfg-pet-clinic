1a3dce63f6dd05a665881a1ed73b64ab0659f670
dmnksdr <dmnksdr@gmail.com>
2018-09-24 15:09:54 +0200

    Add Speciality Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/dmnk/springframework/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package dmnk.springframework.sfgpetclinic.services;
+
+import dmnk.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
